9e2704aeda9ca8bff1ce515c37ff9fcd62dae6b9
Toks Desalu <desalu.developer@gmail.com>
2020-10-19 04:36:08 -0400

    Added /exercise route and tested the route

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const path = require('path');
 
 const PORT = 7000 || process.env.PORT;
 
 const app = express();
 
 app.use(express.static("public"));
+
+app.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/exercise.html'));
+})
 
 app.listen(PORT, (err) => {
   if (err) throw err;
